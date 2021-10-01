622198dfdf5a508c39e79da1a92e417e83fc4f0e
verazb <65012773+verazb@users.noreply.github.com>
2021-10-01 16:30:55 +0200

    copy puma

---
+++ binder/install.R
@@ -0,0 +1,53 @@
+# One line per package 
+
+# Prep 
+install.packages("ggplot2")
+install.packages("tidyverse")
+install.packages("foreign")
+install.packages("haven")
+install.packages("Hmisc")
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("stringr")
+install.packages("dummies")
+install.packages("sandwich")
+install.packages("lmtest")
+install.packages("expss")
+install.packages("fastDummies")
+install.packages("psych")
+install.packages("fBasics")
+install.packages("knitr")
+install.packages("xtable")
+install.packages("sjlabelled")
+install.packages("data.table")
+install.packages("lubridate")
+install.packages("arsenal")
+install.packages("stargazer")
+install.packages("mfx")
+install.packages('devtools')
+install.packages('jtools')
+install.packages('data.table')
+install.packages('etable')
+
+# IV
+install.packages('AER')
+
+# CIA, mediation & IV
+install.packages("causalweight")
+install.packages("np")
+
+# DiD
+install.packages('fixest')
+
+# RDD
+install.packages('grid')
+install.packages('lpdensity')
+install.packages('rddensity')
+install.packages('rdlocrand')
+install.packages('rdrobust')
+install.packages('TeachingDemos')
+install.packages("rdd")
+
+# Quantile
+install.packages("quantreg")
+install.packages("Counterfactual")
